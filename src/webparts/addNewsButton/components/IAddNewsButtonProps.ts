ac7a41f897bd018886b2f5297e28cb141c1478fd
unknown <watanabemi@ad.techvan.co.jp>
2019-02-12 12:29:29 +0900

    delete

--- src/webparts/addNewsButton/components/IAddNewsButtonProps.ts
+++
@@ -1,9 +0,0 @@
-/** ニュース追加ボタンコンポーネント プロパティ定義 */
-export interface IAddNewsButtonProps {
-  
-  /** ボタンクリックイベント
-   * @param pageName - string : ページ名
-   */
-  buttonEvent : (pageName : string) => Promise<void>;
-
-}
